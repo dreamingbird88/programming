959ba6753c7643658f69c57323f07f29d1c9f434
dreamingbird88 <jeffnju@gmail.com>
2017-01-24 20:51:34 -0500

    Modified mscs homework.

--- mscs/ML4T_2017Spring/mc1p2_optimize_portfolio/optimization.py
+++ mscs/ML4T_2017Spring/mc1p2_optimize_portfolio/optimization.py
@@ -2,35 +2,76 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+#import scipy.optimize as spo
 import numpy as np
 import datetime as dt
 from util import get_data, plot_data
+
+
+def obj(allocs, mean, corr, rfr):
+    tmp = np.dot(allocs, mean) - rfr
+    return - tmp * tmp / (allocs.transpose() * corr * allocs)
+
+def normalize(prices):
+    '''
+    Normalize values from stock prices.
+    '''
+    prices.fillna(axis=1, method='bfill', inplace=True)
+    prices.fillna(axis=1, method='ffill', inplace=True)
+    value = prices / prices.iloc[0]
+    return value
+
+def get_stats(values):
+    '''
+    Return mean and correlation matrix of ret.
+    '''
+    values = (values / values.shift(1) - 1)[1:]
+    corr = values.corr().values
+    mean = values.mean().values
+    return mean, corr
+
+def get_perf(values, allocs, rfr, n):
+    '''
+    Get performance metrics for a portfolio.
+    '''
+    value = values * allocs
+    adr = value.mean()
+    sddr = value.std()
+    sr = (mean - rfr) * sqrt(n) / std
+    cr = value.iloc[-1]
+    return sr, adr, sddr, cr, value
 
 # This is the function that will be tested by the autograder
 # The student must update this code to properly implement the functionality
 def optimize_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), \
     syms=['GOOG','AAPL','GLD','XOM'], gen_plot=False):
-
     # Read in adjusted closing prices for given symbols, date range
     dates = pd.date_range(sd, ed)
-    prices_all = get_data(syms, dates)  # automatically adds SPY
-    prices = prices_all[syms]  # only portfolio symbols
-    prices_SPY = prices_all['SPY']  # only SPY, for comparison later
-
-    # find the allocations for the optimal portfolio
-    # note that the values here ARE NOT meant to be correct for a test case
-    allocs = np.asarray([0.2, 0.2, 0.3, 0.3, 0.0]) # add code here to find the allocations
-    cr, adr, sddr, sr = [0.25, 0.001, 0.0005, 2.1] # add code here to compute stats
-
-    # Get daily portfolio value
-    port_val = prices_SPY # add code here to compute daily portfolio values
-
+    prices_all = get_data(syms, dates)[syms+['SPY']]  # automatically adds SPY
+    values = normalize(prices_all)
+    values_SPY = values_SPY['SPY']  # only SPY, for comparison later
+    values = values[syms]  # only portfolio symbols
+    # Optimize portfolio allocations.
+    mean, corr = get_stats(values)
+    stock_num = len(syms)
+    allocs = np.ones((stock_num,1)) / stock_num
+    cons = ({'type': 'eq', 'fun': lambda x:  1 - sum(x)})
+    bnds = tuple([(0,1)] * stock_num)
+    result = spo.minimize(obj, allocs, method=‘SLSQP’, bounds=bnds, constraints=cons)
+    allocs = result['x']
+    # Get the performance of the portfolio.
+    sr, adr, sddr, cr, value = get_perf(values, allocs, rfr, n)
     # Compare daily portfolio value with SPY using a normalized plot
     if gen_plot:
         # add code to plot here
-        df_temp = pd.concat([port_val, prices_SPY], keys=['Portfolio', 'SPY'], axis=1)
+        compare = pd.concat([value, value_SPY], keys=['Portfolio', 'SPY'], axis=1)
+        ax = compare.plot(title="Daily portfolio value and SPY", label='Portfolio')
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Normalized price")
+        plt.legend(loc='upper left')
+        plt.grid()
+        plt.show()
         pass
-
     return allocs, cr, adr, sddr, sr
 
 def test_code():
@@ -45,6 +86,10 @@ def test_code():
     start_date = dt.datetime(2009,1,1)
     end_date = dt.datetime(2010,1,1)
     symbols = ['GOOG', 'AAPL', 'GLD', 'XOM', 'IBM']
+
+sd=dt.datetime(2008,1,1)
+ed=dt.datetime(2009,1,1)
+syms=['GOOG','AAPL','GLD','XOM']
 
     # Assess the portfolio
     allocations, cr, adr, sddr, sr = optimize_portfolio(sd = start_date, ed = end_date,\
